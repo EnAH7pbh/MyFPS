c63bcd853a0ab77d05db40accc589bcc21bf5314
Ahmad Hanif Nurfauzi <hanif_a13@yahoo.com>
2020-01-19 20:01:22 +0700

    Enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+[RequireComponent (typeof (UnityEngine.AI.NavMeshAgent))]
+public class Enemy : MonoBehaviour {
+    public float speed;
+    public int EnemyHealth = 30;
+    public static int EnemyDamage = 1;
+    public static bool GiveDamage = false;
+    GameObject target;
+    public UnityEngine.AI.NavMeshAgent agent { get; private set; }
+    // Start is called before the first frame update
+    void Start () {
+        agent = GetComponent<NavMeshAgent> ();
+        target = GameObject.FindGameObjectWithTag ("Player");
+        agent.speed = speed;
+        agent.updateRotation = true;
+        agent.updatePosition = true;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (target != null) {
+            agent.SetDestination (target.transform.position);
+        }
+    }
+}
