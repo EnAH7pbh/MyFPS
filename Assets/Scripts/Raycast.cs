29ad96c55dda68c018132fe81cfd513fc780a51d
Ahmad Hanif Nurfauzi <hanif_a13@yahoo.com>
2020-01-20 11:49:30 +0700

    laser upgrade 2

--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -4,31 +4,30 @@ using UnityEngine;
 
 public class Raycast : MonoBehaviour {
     public int damage = 1;
-    public float FireRate = 0.25f;
+    private float FireRate;
     public float weaponRange = 50f;
-    public float hitForce = 100;
-    public Transform gunEnd;
+    public float hitForce;
     private Camera fpsCam;
-    private WaitForSeconds shotDuration = new WaitForSeconds (0.7f);
+    private WaitForSeconds shotDuration;
     private AudioSource gunAudio;
-    private LineRenderer laserLine;
+    public ParticleSystem laserLine;
     private float nextFire;
     void Start () {
-        laserLine = GetComponent<LineRenderer> ();
         gunAudio = GetComponent<AudioSource> ();
         fpsCam = GetComponentInParent<Camera> ();
+        shotDuration = new WaitForSeconds (laserLine.main.duration);
+        FireRate = laserLine.main.duration;
     }
 
     void Update () {
-        if (Input.GetButtonDown ("Fire1") && Time.time > nextFire) {
+        if (Input.GetMouseButtonDown(0) && Time.time > nextFire) {
             nextFire = Time.deltaTime + FireRate;
             StartCoroutine (shotEffect ());
             Vector3 rayOrigin = fpsCam.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, 0));
             RaycastHit hit;
-            laserLine.SetPosition (0, gunEnd.position);
             if (Physics.Raycast (rayOrigin, fpsCam.transform.forward, out hit, weaponRange)) {
-                laserLine.SetPosition (1, hit.point);
                 Shootable health = hit.collider.GetComponent<Shootable> ();
+                laserLine.transform.LookAt (hit.point);
                 if (health != null) {
                     health.Damage (damage);
                 }
@@ -36,14 +35,13 @@ public class Raycast : MonoBehaviour {
                     hit.rigidbody.AddForce (-hit.normal * hitForce);
                 }
             } else {
-                laserLine.SetPosition (1, rayOrigin + (fpsCam.transform.forward * weaponRange));
+                laserLine.transform.LookAt (rayOrigin + (fpsCam.transform.forward * weaponRange));
             }
         }
     }
     private IEnumerator shotEffect () {
         gunAudio.Play ();
-        laserLine.enabled = true;
+        laserLine.Play ();
         yield return shotDuration;
-        laserLine.enabled = false;
     }
 }
